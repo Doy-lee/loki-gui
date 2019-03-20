d10cc1c092573ec7ce97ad42f148189f84ed77b0
Doyle <Doy-lee@users.noreply.github.com>
2019-03-20 16:11:59 +1000

    issue_1: fix push error (#86)

--- get_libwallet_api.sh
+++ get_libwallet_api.sh
@@ -211,12 +211,13 @@ eval make -C $LOKI_DIR/build/$BUILD_TYPE/external/easylogging++ all install
 eval make -C $LOKI_DIR/build/$BUILD_TYPE/external/db_drivers/liblmdb all install
 
 # Install libunbound
-echo "Installing libunbound..."
-pushd $LOKI_DIR/build/$BUILD_TYPE/external/unbound
-# no need to make, it was already built as dependency for libwallet
-# make -j$CPU_CORE_COUNT
-$make_exec install -j$CPU_CORE_COUNT
-popd
-
+if [ -d $LOKI_DIR/build/$BUILD_TYPE/external/unbound ]; then
+    echo "Installing libunbound..."
+    pushd $LOKI_DIR/build/$BUILD_TYPE/external/unbound
+    # no need to make, it was already built as dependency for libwallet
+    # make -j$CPU_CORE_COUNT
+    $make_exec install -j$CPU_CORE_COUNT
+    popd
+fi
 
 popd
